3ae7792540400a3d212ad5b476d2dc8359e8f0cc
Michael Kelly <mkelly@mozilla.com>
2011-09-07 14:26:31 -0700

    Fix up form validation.

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -1,4 +1,7 @@
+import re
+
 from django import forms
+from django.forms.util import ValidationError
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
 
@@ -8,18 +11,42 @@ from tower import ugettext_lazy as _lazy
 from users.models import UserProfile
 
 
+PASSWD_MATCH = _lazy('Passwords must match.')
+PASSWD_LENGTH = _lazy('Passwords must be at least 8 characters long.')
+PASSWD_COMPLEX = _lazy('Passwords must contain at least 1 letter and '
+                       '1 number.')
+
+
 class PasswordField(forms.CharField):
     """Field for displaying a password input."""
-    def __init__(self, label=_lazy(u'Password'),
-                 widget=forms.PasswordInput(render_value=False),
-                 min_length=6, max_length=255, *args, **kwargs):
-        return super(PasswordField, self).__init__(label=label, widget=widget,
-                                                   min_length=min_length,
-                                                   max_length=max_length,
-                                                   *args, **kwargs)
+
+    error_messages = {
+        'min_length': PASSWD_LENGTH,
+    }
+
+    def __init__(self, *args, **kwargs):
+        defaults = {
+            'label': _lazy(u'Password'),
+            'widget': forms.PasswordInput(render_value=False),
+            'min_length': 8,
+            'error_messages': PasswordField.error_messages,
+        }
+        defaults.update(kwargs)
+        return super(PasswordField, self).__init__(*args, **defaults)
+
+    def clean(self, value):
+        """Passwords must consist of letters and numbers."""
+        cleaned_value = super(PasswordField, self).clean(value)
+
+        # Unicode letters are anything in \w that isn't a digit or underscore
+        letters = re.search(r'(?Lu)[^\W\d_]+', cleaned_value)
+        numbers = re.search(r'(?Lu)\d+', cleaned_value)
+        if not (letters and numbers):
+            raise ValidationError(PASSWD_COMPLEX)
+
+        return cleaned_value
 
 
-# TODO: Confirm constraints on user data
 # TODO: Prevent common passwords
 class RegisterForm(forms.Form):
     """Form used to create registration profile."""
@@ -39,10 +66,10 @@ class ProfileForm(forms.ModelForm):
         model = UserProfile
 
     placeholders = {
-        'address_1': _('Street'),
-        'address_2': _('Apartment or Unit # (optional)'),
-        'city': _('City'),
-        'state': _('State or Province'),
+        'address_1': _lazy('Street'),
+        'address_2': _lazy('Apartment or Unit # (optional)'),
+        'city': _lazy('City'),
+        'state': _lazy('State or Province'),
     }
 
     def __init__(self, *args, **kwargs):
@@ -59,25 +86,16 @@ class ProfileForm(forms.ModelForm):
 
 class EditProfileForm(ProfileForm):
     """Form for editing an existing UserProfile."""
-    email = forms.EmailField(label=_lazy(u'Email'))
     password = PasswordField(label=_lazy(u'New Password'), required=False)
     password2 = PasswordField(label=_lazy(u'Retype Password'), required=False)
 
     class Meta(ProfileForm.Meta):
         exclude = ('user', 'modified', 'created')
 
-    def __init__(self, *args, **kwargs):
-        super(EditProfileForm, self).__init__(*args, **kwargs)
-
-        # Pull email from user object if possible
-        if self.instance:
-            self.initial['email'] = self.instance.user.email
-
     def save(self, *args, **kwargs):
         """Save email and password to user object instead of UserProfile."""
         if self.is_valid():
             user = self.instance.user
-            user.email = self.cleaned_data['email']
             if self.cleaned_data['password']:
                 user.set_password(self.cleaned_data['password'])
 
@@ -91,7 +109,7 @@ class EditProfileForm(ProfileForm):
         password2 = self.cleaned_data.get('password2')
 
         if not password == password2:
-            raise forms.ValidationError(_('Passwords must match.'))
+            self._errors['password'] = self.error_class([PASSWD_MATCH])
         elif password:
             self.cleaned_data['password'] = password
 
