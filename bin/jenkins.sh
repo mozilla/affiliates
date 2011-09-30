ddffab840f9cc5578836ed1842ba9e80e9fc92a1
Dave Dash <dd+github@davedash.com>
2011-09-29 19:27:40 -0700

    ZOMG no site packages jenkins, 688874

--- bin/jenkins.sh
+++ bin/jenkins.sh
@@ -14,7 +14,7 @@ find . -name '*.pyc' | xargs rm
 
 if [ ! -d "$VENV/bin" ]; then
   echo "No virtualenv found.  Making one..."
-  virtualenv $VENV
+  virtualenv $VENV --no-site-packages
   source $VENV/bin/activate
   pip install --upgrade pip
   pip install coverage
