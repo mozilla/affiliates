6d86ceb8adf006f84576598b5f8942904c3c7fa5
Michael Kelly <mkelly@mozilla.com>
2011-11-14 15:51:01 -0800

    Add the color purple to available colors.
    This will be localized at a later date.

--- apps/banners/__init__.py
+++ apps/banners/__init__.py
@@ -1,6 +1,7 @@
 from tower import ugettext as _
 
 
-COLORS = ['Blue', 'Green', 'Red', 'Orange', 'Yellow', 'White', 'Black']
+COLORS = ['Blue', 'Green', 'Red', 'Orange', 'Yellow', 'White', 'Black',
+          'Purple']
 LOCALIZED_COLORS = [_(color) for color in COLORS]
 COLOR_CHOICES = zip(COLORS, LOCALIZED_COLORS)
