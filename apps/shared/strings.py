783c5cfbec248c5063d2c7bd3967842860a87c17
Michael Kelly <mkelly@mozilla.com>
2011-11-14 15:41:03 -0800

    Store database strings in a file.
    Because extracting from the database became difficult to maintain,
    we're moving strings into a file as part of the project. We're also
    removing the command to extract from the database.

---
+++ apps/shared/strings.py
@@ -0,0 +1,42 @@
+"""
+Contains strings that will be inserted into the DB that need to be
+localized.
+"""
+from tower import ugettext as _
+
+# TODO: Move these into separate files when updating becomes unwieldy.
+
+# News
+_('The new Firefox Affiliates program is live!')
+_('Welcome to the new Firefox Affiliates program. Have a look around the '
+  'site, grab some buttons and get on the leaderboard. Have feedback? Let us '
+  'know at affiliates@mozilla.org.')
+
+# Categories
+_('Firefox')
+_('Mozilla')
+
+# Subcategories
+_('Mozilla Recruiting')
+_('Firefox <span>for</span> Mobile')
+_('Firefox <span>for</span> Desktop')
+_('Firefox Plugins')
+
+# Banners
+_('Android')
+_('Upgrade')
+_('Download: Fast, Fun, Awesome')
+_('Work <span>for</span> Mozilla')
+_('Plugin Checker')
+_('Download Aurora')
+_('Aurora <span>for</span> Android')
+
+# Colors
+_('Purple')
+_('Blue')
+_('Green')
+_('Red')
+_('Orange')
+_('Yellow')
+_('White')
+_('Black')
