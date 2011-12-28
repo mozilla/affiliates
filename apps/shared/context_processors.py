ad98b6bcafa58fb074bfc3c00c74af67edd1430a
Michael Kelly <mkelly@mozilla.com>
2011-12-28 09:10:15 -0800

    Add check in l10n context processor.

--- apps/shared/context_processors.py
+++ apps/shared/context_processors.py
@@ -5,7 +5,7 @@ from django.utils.translation import get_language
 def l10n(request):
     """Adds language information to template contexts."""
     locale = get_language()
-    if locale:
+    if locale and locale in settings.LANGUAGES:
         return {'LOCALE': locale, 'LANGUAGE': settings.LANGUAGES[locale]}
     else:
         return {}
