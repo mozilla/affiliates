15316a5da3ff6e209202e297d67e0968501c7e95
Michael Kelly <mkelly@mozilla.com>
2012-01-30 12:18:50 -0800

    Simplyfy and fix bugs in OverwritingStorage.
    bug 715563

--- apps/shared/storage.py
+++ apps/shared/storage.py
@@ -1,18 +1,17 @@
-# Taken from http://djangosnippets.org/snippets/2173/
-
 import os
-import errno
-import tempfile
+from tempfile import mkstemp
 
 from django.conf import settings
 from django.core.files import locks
 from django.core.files.move import file_move_safe
-from django.core.files.storage import FileSystemStorage, Storage
+from django.core.files.storage import FileSystemStorage
 from django.utils.text import get_valid_filename
 
 class OverwritingStorage(FileSystemStorage):
     """
     File storage that allows overwriting of stored files.
+
+    Modified from http://djangosnippets.org/snippets/2173/
     """
 
     def get_available_name(self, name):
@@ -30,34 +29,24 @@ class OverwritingStorage(FileSystemStorage):
         elif not os.path.isdir(directory):
             raise IOError("%s exists and is not a directory." % directory)
 
+        # Ensure that content is open
         content.open()
-        # This file has a file path that we can move.
+
         if hasattr(content, 'temporary_file_path'):
+            # Content has a file that we can move.
             temp_data_location = content.temporary_file_path()
+            file_move_safe(temp_data_location, full_path)
         else:
-            tmp_prefix = "tmp_%s" %(get_valid_filename(name), )
-            temp_data_location = tempfile.mktemp(prefix=tmp_prefix,
-                                                 dir=self.location)
-            try:
-                # This is a normal uploadedfile that we can stream.
-                # This fun binary flag incantation makes os.open throw an
-                # OSError if the file already exists before we open it.
-                fd = os.open(temp_data_location,
-                             os.O_WRONLY | os.O_CREAT |
-                             os.O_EXCL | getattr(os, 'O_BINARY', 0))
-                locks.lock(fd, locks.LOCK_EX)
-                for chunk in content.chunks():
-                    os.write(fd, chunk)
-                locks.unlock(fd)
-                os.close(fd)
-            except Exception, e:
-                if os.path.exists(temp_data_location):
-                    os.remove(temp_data_location)
-                raise
+            # Write the content stream to a temporary file and move it.
+            fd, tmp_path = mkstemp()
+            locks.lock(fd, locks.LOCK_EX)
+            for chunk in content.chunks():
+                os.write(fd, chunk)
+            locks.unlock(fd)
+            os.close(fd)
+            file_move_safe(tmp_path, full_path)
 
-        file_move_safe(temp_data_location, full_path)
         content.close()
-
         if settings.FILE_UPLOAD_PERMISSIONS is not None:
             os.chmod(full_path, settings.FILE_UPLOAD_PERMISSIONS)
 
