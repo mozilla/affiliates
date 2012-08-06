2240f18354ad81e6e8de3ce3fe5138ddc406cde0
Michael Kelly <mkelly@mozilla.com>
2012-08-06 13:49:28 -0400

    Lazy-eval MIDDLEWARE_CLASSES to add middleware.

--- settings/__init__.py
+++ settings/__init__.py
@@ -6,6 +6,23 @@ except ImportError, exc:
     exc.args = tuple(['%s (did you rename settings/local.py-dist?)' % exc.args[0]])
     raise exc
 
+
+# Lazily evaluate MIDDLEWARE_CLASSES in order to add middleware based on
+# settings like DEBUG or DEV.
+# We do this here instead of base.py to avoid having to refer to
+# STATIC_MIDDLEWARE_CLASSES everywhere in settings instead of the default.
+def lazy_middleware_classes():
+    from django.conf import settings
+    middleware = settings.STATIC_MIDDLEWARE_CLASSES
+
+    if settings.DEBUG:
+        middleware.insert(0, 'facebook.middleware.FacebookDebugMiddleware')
+
+    return middleware
+STATIC_MIDDLEWARE_CLASSES = MIDDLEWARE_CLASSES[:]
+MIDDLEWARE_CLASSES = lazy(lazy_middleware_classes, list)()
+
+
 # Import test settings
 TEST = len(sys.argv) > 1 and sys.argv[1] == 'test'
 if TEST:
