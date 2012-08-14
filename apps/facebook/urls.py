a89bedd371c709cb634136cc23ccc678562b8df8
Michael Kelly <mkelly@mozilla.com>
2012-08-14 14:10:09 -0700

    Make urls.py a little more readable.

--- apps/facebook/urls.py
+++ apps/facebook/urls.py
@@ -5,10 +5,15 @@ from facebook import views
 
 urlpatterns = patterns('facebook.views',
     url(r'^/?$', views.load_app, name='facebook.load_app'),
-    url(r'^create_banner/?$', views.create_banner, name='facebook.create_banner'),
+
+    url(r'^create_banner/?$', views.create_banner,
+        name='facebook.create_banner'),
+
     url(r'^banner_list/?$', views.banner_list, name='facebook.banner_list'),
+
     url(r'^post_banner_share/?$', views.post_banner_share,
         name='facebook.post_banner_share'),
+
     url(r'^link_accounts/?$', views.link_accounts,
         name='facebook.link_accounts'),
 )
