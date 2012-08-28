145d83cf63705d71cc04d3297017b8f1b7c1cec2
Michael Kelly <mkelly@mozilla.com>
2012-08-28 15:04:15 -0400

    Fix leaderboard update test.
    The bug in the leaderboard update was uncovered by accounts where
    the creation order did not match the leaderboard order. To
    improve the leaderboard update test, this changes the leaderboard
    to mimic that condition.

--- apps/facebook/tests/test_cron.py
+++ apps/facebook/tests/test_cron.py
@@ -11,9 +11,9 @@ class UpdateFacebookLeaderboardTests(TestCase):
         """Test a basic run of the update leaderboard cron command."""
         # Chains through factories to create two seperate users.
         stats1 = (FacebookClickStatsFactory
-                  .create(clicks=4, banner_instance__total_clicks=4))
-        stats2 = (FacebookClickStatsFactory
                   .create(clicks=2, banner_instance__total_clicks=2))
+        stats2 = (FacebookClickStatsFactory
+                  .create(clicks=4, banner_instance__total_clicks=4))
         user1 = stats1.banner_instance.user
         user2 = stats2.banner_instance.user
 
@@ -22,7 +22,7 @@ class UpdateFacebookLeaderboardTests(TestCase):
         user1 = FacebookUser.objects.get(id=user1.id)
         user2 = FacebookUser.objects.get(id=user2.id)
 
-        eq_(user1.leaderboard_position, 1)
-        eq_(user2.leaderboard_position, 2)
-        eq_(user1.total_clicks, 4)
-        eq_(user2.total_clicks, 2)
+        eq_(user1.leaderboard_position, 2)
+        eq_(user2.leaderboard_position, 1)
+        eq_(user1.total_clicks, 2)
+        eq_(user2.total_clicks, 4)
