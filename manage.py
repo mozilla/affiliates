fd80230aee25c1a72971a8a1be0243965c370079
Michael Kelly <mkelly@mozilla.com>
2014-01-09 16:35:19 -0500

    Update manage.py and playdoh.wsgi to match new playdoh.

--- manage.py
+++ manage.py
@@ -2,10 +2,14 @@
 import os
 import sys
 
+# Edit this if necessary or override the variable in your environment.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'affiliates.settings')
+
 # Add a temporary path so that we can import the funfactory
 tmp_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                         'vendor', 'src', 'funfactory')
-sys.path.append(tmp_path)
+# Comment out to load funfactory from your site packages instead
+sys.path.insert(0, tmp_path)
 
 from funfactory import manage
 
@@ -13,7 +17,7 @@ from funfactory import manage
 sys.path.remove(tmp_path)
 
 
-manage.setup_environ(__file__)
+manage.setup_environ(__file__, more_pythonic=True)
 
 if __name__ == "__main__":
     manage.main()
--- wsgi/playdoh.wsgi
+++ wsgi/playdoh.wsgi
@@ -1,8 +1,9 @@
 import os
 import site
 
-os.environ['CELERY_LOADER'] = 'django'
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+os.environ.setdefault('CELERY_LOADER', 'django')
+# NOTE: you can also set DJANGO_SETTINGS_MODULE in your environment to override
+# the default value in manage.py
 
 # Add the app dir to the python path so we can import manage.
 wsgidir = os.path.dirname(__file__)
