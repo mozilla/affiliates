327ae089297795c7deecec113144e385a40c00e7
Michael Kelly <mkelly@mozilla.com>
2014-04-24 23:27:35 -0400

    Disable submit button on customize screens until option chosen.

--- affiliates/banners/static/js/customize_text_banner.js
+++ affiliates/banners/static/js/customize_text_banner.js
@@ -3,14 +3,22 @@
     'use strict';
 
     var $form = $('#customize-banner-form');
+    var $preview = $('#text-preview');
+    var $submit = $form.find('button[type="submit"]');
 
     // When the variation selectbox changes, update the adjacent preview.
     $(document).on('change', '#customize-banner-form select[name="variation"]', function(e) {
         var $select = $(this);
         var variationsText = $form.data('variationsText');
-        var $preview = $('#text-preview');
 
-        $preview.text(variationsText[$select.val()]);
+        var text = variationsText[$select.val()];
+        if (text) {
+            $preview.text(text);
+            $submit.prop('disabled', false);
+        } else {
+            $preview.text('');
+            $submit.prop('disabled', true);
+        }
     });
 
     // Trigger change event once on page load to populate.
