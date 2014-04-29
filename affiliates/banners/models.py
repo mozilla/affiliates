a8ecf98ca9c960cabcf2a33428b31829eaa4a691
Giorgos Logiotatidis <seadog@sealabs.net>
2014-04-29 14:58:07 +0300

    Add verbose_name_plural for Category.

--- affiliates/banners/models.py
+++ affiliates/banners/models.py
@@ -29,6 +29,9 @@ class Category(MPTTModel):
     name = models.CharField(max_length=255)
     parent = TreeForeignKey('self', null=True, blank=True, related_name='children')
 
+    class Meta:
+        verbose_name_plural = 'categories'
+
     class MPTTMeta:
         order_insertion_by = ['name']
 
