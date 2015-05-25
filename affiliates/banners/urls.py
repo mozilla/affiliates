7f25dd131975a892ebff05df32a7668514957996
Giorgos Logiotatidis <seadog@sealabs.net>
2015-05-25 13:21:13 +0300

    [bug 1159515] Don't allow new user creation.
    [bug 1159515] Don't allow new banner creation.
    
    Don't include banner section when no banners.

--- affiliates/banners/urls.py
+++ affiliates/banners/urls.py
@@ -4,19 +4,19 @@ from affiliates.banners import views
 
 
 urlpatterns = patterns('',
-    url(r'^generator/categories/$', views.CategoryListView.as_view(),
+    url(r'^generator/categories/$', views.NoMoreBannersRedirectView.as_view(),
         name='banners.generator.categories'),
 
-    url(r'^generator/categories/(?P<category_pk>\d+)/$', views.BannerListView.as_view(),
+    url(r'^generator/categories/(?P<category_pk>\d+)/$', views.NoMoreBannersRedirectView.as_view(),
         name='banners.generator.banners'),
 
     url(r'^generator/image_banners/(?P<pk>\d+)/customize/$',
-        views.CustomizeImageBannerView.as_view(), name='banners.generator.image_banner.customize'),
+        views.NoMoreBannersRedirectView.as_view(), name='banners.generator.image_banner.customize'),
 
     url(r'^generator/text_banners/(?P<pk>\d+)/customize/$',
-        views.CustomizeTextBannerView.as_view(), name='banners.generator.text_banner.customize'),
+        views.NoMoreBannersRedirectView.as_view(), name='banners.generator.text_banner.customize'),
 
     url(r'^generator/firefox_upgrade_banners/(?P<pk>\d+)/customize/$',
-        views.CustomizeFirefoxUpgradeBannerView.as_view(),
+        views.NoMoreBannersRedirectView.as_view(),
         name='banners.generator.firefox_upgrade_banner.customize'),
 )
